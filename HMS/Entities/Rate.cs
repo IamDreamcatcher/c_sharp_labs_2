3d2919f9c5164463cca359fd894b2eca31251f37
IamDreamcatcher <OlegVolkovskiy2002@yandex.ru>
2021-09-18 19:36:05 +0300

    HMS

---
+++ HMS/Entities/Rate.cs
@@ -0,0 +1,13 @@
+﻿namespace HMS.Entities
+{
+    class Rate
+    {
+        public string name;
+        public int cost;
+        public Rate(string name, int cost)
+        {
+            this.name = name;
+            this.cost = cost;
+        }
+    }
+}
---
+++ HMS/Entities/Tenant.cs
@@ -0,0 +1,13 @@
+﻿namespace HMS.Entities
+{
+    class Tenant
+    {
+        public string name;
+        public int spending;
+        public Tenant(string name, int spending)
+        {
+            this.name = name;
+            this.spending = spending;
+        }
+    }
+}
